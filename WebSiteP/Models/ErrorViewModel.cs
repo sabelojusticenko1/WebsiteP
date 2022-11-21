1c71bda920e8827c0a5bab78977a72fd828bfb0e
sabelojusticenko1@gmail.com <sabelojusticenko1@gmail.com>
2022-11-21 09:40:03 +0200

    New

---
+++ WebSiteP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebSiteP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
